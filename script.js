cfd16499b1693f96ca04fed16dbedeed582b346b
Mauricio <mauryvillalba@gmail.com>
2020-06-08 23:09:36 -0300

    los primeros 12 pokemon cargan id e imagen

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const showMoreInfo = () => {
-  alert("hola");
-}
+$( document ).ready(function() {
+  
+  const showMoreInfo = () => {
+    alert("hola");
+  }
+  //cargar las primeras imagenes
+  $(".pokemon-id").map(function (index) {
+    this.innerText = `Id: #${index<=9?'0'+index:index}`
+  })
+  $(".card img").map(function(index){$(this).attr('src',`https://pokeres.bastionbot.org/images/pokemon/${index+1}.png`)})
+
+});
